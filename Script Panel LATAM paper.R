e3c2db222228762215c7df223c795e950e4cfbc8
jdalonsos <jdalonsos@unal.edu.co>
2022-10-27 21:42:35 -0500

    .R script LATAM panel Data

---
+++ Script Panel LATAM paper.R
@@ -0,0 +1,15 @@
+#Read data
+library(readxl)
+base_panel=read_xlsx(path = "C:/Users/juand/OneDrive/Escritorio/Daniel/datalogARG&EC.xlsx")
+library(foreign)
+library(carData)
+library(car)
+x11()
+par(mfrow=c(3,3))
+scatterplot(co2epik~Año|Pais,boxplots=FALSE, smooth=TRUE, reg.line=FALSE, data=base_panel)
+scatterplot(NONREN~Año|Pais,boxplots=FALSE, smooth=TRUE, reg.line=FALSE, data=base_panel)
+scatterplot(LOWENER~Año|Pais,boxplots=FALSE, smooth=TRUE, reg.line=FALSE, data=base_panel)
+scatterplot(FOR~Año|Pais,boxplots=FALSE, smooth=TRUE, reg.line=FALSE, data=base_panel)
+scatterplot(KOFGI~Año|Pais,boxplots=FALSE, smooth=TRUE, reg.line=FALSE, data=base_panel)
+scatterplot(AVA~Año|Pais,boxplots=FALSE, smooth=, reg.line=FALSE, data=base_panel)
+
