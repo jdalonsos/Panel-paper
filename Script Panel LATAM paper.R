4de8b850724c50350f780875361a8f9f7c025391
jdalonsos <jdalonsos@unal.edu.co>
2022-10-30 14:07:36 -0500

    graphs

--- Script Panel LATAM paper.R
+++ Script Panel LATAM paper.R
@@ -4,6 +4,7 @@ base_panel=read_xlsx(path = "C:/Users/juand/OneDrive/Escritorio/Daniel/datalogAR
 library(foreign)
 library(carData)
 library(car)
+library(ggplot2)
 x11()
 par(mfrow=c(3,3))
 scatterplot(co2epik~Año|Pais,boxplots=FALSE, smooth=TRUE, reg.line=FALSE, data=base_panel)
@@ -12,4 +13,10 @@ scatterplot(LOWENER~Año|Pais,boxplots=FALSE, smooth=TRUE, reg.line=FALSE, data=
 scatterplot(FOR~Año|Pais,boxplots=FALSE, smooth=TRUE, reg.line=FALSE, data=base_panel)
 scatterplot(KOFGI~Año|Pais,boxplots=FALSE, smooth=TRUE, reg.line=FALSE, data=base_panel)
 scatterplot(AVA~Año|Pais,boxplots=FALSE, smooth=, reg.line=FALSE, data=base_panel)
+
+ggplot(data = base_panel_levels, aes(x = Año, y = co2epik)) +
+  geom_line(aes(colour = as.factor(Pais))) +
+  geom_smooth(method = "lm", se = F, lty = "dashed") +
+  labs(x = "Year",  y = "Gross co2epik") +
+  theme(legend.position = "bottom")
 
